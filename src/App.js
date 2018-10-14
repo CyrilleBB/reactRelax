6ef9be8137e3e24c05c146c02b6f55cd53d367ea
masajama <masajama@protonmail.com>
2018-10-14 21:53:29 +0200

    boutons images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import { BrowserRouter, Route } from "react-router-dom";
+import pluie from "./assets/img/pluie.jpg";
+import foret from "./assets/img/foret.jpg";
+import campagne from "./assets/img/campagne.png";
+import montagne from "./assets/img/montagne.jpg";
+import mer from "./assets/img/mer.jpg";
 
 class App extends Component {
+  state = {
+    imageURL: pluie
+  };
+
+  changeImage = uneImage => {
+    this.setState({
+      imageURL: uneImage
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img id="background" src={this.state.imageURL} alt="pluie" />
+
+        <button
+          onClick={() => {
+            this.changeImage(montagne);
+          }}
+        >
+          Montagne
+        </button>
+        <button
+          onClick={() => {
+            this.changeImage(mer);
+          }}
+        >
+          mer
+        </button>
+        <button
+          onClick={() => {
+            this.changeImage(campagne);
+          }}
+        >
+          campagne
+        </button>
+        <button
+          onClick={() => {
+            this.changeImage(foret);
+          }}
+        >
+          foret
+        </button>
+        <button
+          onClick={() => {
+            this.changeImage(pluie);
+          }}
+        >
+          pluie
+        </button>
       </div>
     );
   }
