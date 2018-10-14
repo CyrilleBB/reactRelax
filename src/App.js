5d688c7b7f2f40362beef8be228f1ee9ce885e81
masajama <masajama@protonmail.com>
2018-10-14 23:02:59 +0200

    ajout son et cercle

--- src/App.js
+++ src/App.js
@@ -1,62 +1,81 @@
 import React, { Component } from "react";
 // import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
 import pluie from "./assets/img/pluie.jpg";
 import foret from "./assets/img/foret.jpg";
 import campagne from "./assets/img/campagne.png";
 import montagne from "./assets/img/montagne.jpg";
 import mer from "./assets/img/mer.jpg";
+import pluieSon from "./assets/audio/pluieSon.mp3";
+import foretSon from "./assets/audio/foretSon.mp3";
+import campagneSon from "./assets/audio/campagneSon.mp3";
+import montagneSon from "./assets/audio/montagneSon.mp3";
+import merSon from "./assets/audio/merSon.mp3";
 
 class App extends Component {
   state = {
-    imageURL: pluie
+    imageURL: pluie,
+    sonUrl: pluieSon
   };
 
-  changeImage = uneImage => {
+  change = (uneImage, unSon) => {
     this.setState({
-      imageURL: uneImage
+      imageURL: uneImage,
+      sonUrl: unSon
     });
   };
 
   render() {
     return (
       <div className="App">
-        <img id="background" src={this.state.imageURL} alt="pluie" />
-
-        <button
-          onClick={() => {
-            this.changeImage(montagne);
-          }}
-        >
-          Montagne
-        </button>
-        <button
-          onClick={() => {
-            this.changeImage(mer);
-          }}
-        >
-          mer
-        </button>
-        <button
-          onClick={() => {
-            this.changeImage(campagne);
-          }}
-        >
-          campagne
-        </button>
-        <button
-          onClick={() => {
-            this.changeImage(foret);
-          }}
-        >
-          foret
-        </button>
-        <button
-          onClick={() => {
-            this.changeImage(pluie);
-          }}
-        >
-          pluie
-        </button>
+        <div className="container">
+          <div id="boite">
+            <div id="cercle">
+              <div id="souffle">
+                <p id="respire" />
+              </div>
+            </div>
+          </div>
+          <img id="background" src={this.state.imageURL} alt="pluie" />
+          <audio src={this.state.sonUrl} autoPlay loop />
+          <ul>
+            <li
+              onClick={() => {
+                this.change(montagne, montagneSon);
+              }}
+            >
+              Montagne
+            </li>
+            <li
+              onClick={() => {
+                this.change(mer, merSon);
+              }}
+            >
+              mer
+            </li>
+            <li
+              onClick={() => {
+                this.change(campagne, campagneSon);
+              }}
+            >
+              campagne
+            </li>
+            <li
+              onClick={() => {
+                this.change(foret, foretSon);
+              }}
+            >
+              foret
+            </li>
+            <li
+              onClick={() => {
+                this.change(pluie, pluieSon);
+              }}
+            >
+              pluie
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
